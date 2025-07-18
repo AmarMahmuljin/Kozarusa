420a35ed47963e78af12a2f0f889d38992039a47
AmarMahmuljin1 <amar.mahmuljin@student.ucll.be>
2025-07-18 15:12:39 +0200

    back-end initialized, app.ts created

---
+++ back-end/app.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const app = express();
+dotenv.config();
+const port = process.env.APP_PORT || 3000;
+
+app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(bodyParser.json());
+
+app.get('/status', (req, res) => {
+    res.json({ message: 'Kozarac API is running...' });
+});
+
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Courses API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./controller/*.routes.ts'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(port || 3000, () => {
+    console.log(`Kozarac API is running on port ${port}.`);
+});
